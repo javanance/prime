1fec8308284ad725ae274b561485e6e13750fa34
javanance <javanance@gmail.com>
2023-09-18 20:51:33 +0900

    root post

---
+++ app/posts/layout.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from "next";
+import Layout from "../../layout/layout";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+  title: "Gbook Ultima",
+  description: "The ultimate archives for risk management.",
+  robots: { index: false, follow: false },
+  viewport: { initialScale: 1, width: "device-width" },
+  openGraph: {
+    type: "website",
+    title: "Gbook with prime",
+    url: "https://www.primefaces.org/ultima-react",
+    description: "The archives for risk management.",
+    images: ["https://www.primefaces.org/static/social/ultima-react.png"],
+    ttl: 604800,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  return <Layout>{children}</Layout>;
+
+  // return <>                {children}           </>
+}
